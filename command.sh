404af959dcb5a28a1cb8f24547d159c274d4f1f0
SJayYang <stanley.j.yang@gmail.com>
2023-08-07 20:10:34 -0700

    Basic bash script to run classification task

---
+++ command.sh
@@ -0,0 +1,1 @@
+python main.py --data_dir /deep2/u/sjayyang/tfrecords/tfrecords_fake_out/class_2 --csv_file /deep2/u/sjayyang/hi3 --model_name ViT --outcome Mean_BMI_bin --epoch_n 10 --batch_size 64 --n_classes 3 --file /deep/u/sjayyang/results.txt
